f409169b539e6ad681a9b79c83a09e0264216467
s147682 <tmrgast@live.nl>
2017-12-26 01:05:07 +0100

    loads of style and html changes

--- places/static/js/main.js
+++ places/static/js/main.js
@@ -10,10 +10,11 @@ $(document).ready(function(){
   });
 
   $(document).on('click', '.mapbutton', function() {
-    var val1 = $(this).siblings('h2').text().split(' ').join('+');
+    // var val1 = $(this).siblings('h2').text().split(' ').join('+');
+    var val1 = $(this).closest('.card').find('#location').text().split(' ').join('+');
     var str = "https://maps.googleapis.com/maps/api/staticmap?center="+ val1 + "&zoom=12&scale=2&size=600x300&maptype=terrain&key=AIzaSyCdJieNVjZkVK1snuD_zRrruMd-ktMhZBU&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7C" + val1;
-
-    $(".map").toggle(400).attr("src",str); //toggle map instead of seperate hide/show
+    console.log(val1);
+    $(".map").toggle().attr("src",str); //toggle map instead of seperate hide/show
     return false;
   });
 });
