ec6785ccf61f354f5d0152692d456c158f53cd39
s147682 <tmrgast@live.nl>
2017-12-27 22:53:36 +0100

    loading symbol thats shows when maps are loading

--- places/static/js/main.js
+++ places/static/js/main.js
@@ -1,5 +1,9 @@
 let container_dark = document.getElementById('dark-container');
 let this_butt = null;
+// store loading image, detach immediately
+let loading = $(".loading");
+$(".loading").detach();
+let map = $(".map");
 
 window.onclick = function(event) {
     if (event.target == container_dark) {
@@ -14,6 +18,7 @@ function disable_input() {
 }
 
 $(document).ready(function(){
+
   $('.info-button').on('click', function() {
     $('.infodiv').toggle();
     return false;
@@ -31,26 +36,38 @@ $(document).ready(function(){
   $(document).on('click', '.mapbutton', function() {
     let new_butt = $(this);
 
+
+
     if (new_butt.is(this_butt)) {
+
       // hide map when same button is pressed time
-      if ($(".map").is(":visible")) {
-        $(".map").hide();
+      if (jQuery.contains(document, map[0])) {
+        $(".map").detach();
         $(this).val("Show Map");
       } else {
-        $(".map").show();
+        $(".map-container").append(map);
         $(this).val("Hide Map");
       }
     } else {
-      if (this_butt){
+      if (this_butt) {
+        // change value of previously pressed button
         this_butt.val("Show Map");
       }
 
+      $(".map").detach();
+      $(".map-container").append(loading);
+
+      this_butt = $(this);
       let val1 = $(this).closest('.card').find('#location').text().split(' ').join('+');
       let str = "https://maps.googleapis.com/maps/api/staticmap?center="+ val1 + "&zoom=12&scale=2&size=600x300&maptype=terrain&key=AIzaSyCdJieNVjZkVK1snuD_zRrruMd-ktMhZBU&format=jpg&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7C" + val1;
 
-      this_butt = $(this);
       this_butt.val("Hide Map");
-      $(".map").show().attr("src",str);
+      $(".map-container").append(map.attr("src",str));
+      map.hide();
+      map.on('load', function() {
+        $(".loading").detach();
+        map.show();
+      });
     }
 
     return false;
