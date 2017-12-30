81cfe61b9bf652796a59e2435bd37785a18146f2
Smudgy <tmrgast@live.nl>
2017-12-31 00:24:09 +0100

    map improvements, intializes once, use other functions for panning/zooming etc.

--- places/static/js/map.js
+++ places/static/js/map.js
@@ -1,97 +1,152 @@
-let current = 0;
-let locs = [];
-// coordinates for when no memories are available (= cool)
-let coordinates = [
-  [2.33, 48.87],
-  [50.43, 30.52],
-  [52.35, 4.92],
-  [38.88,-77],
-  [45.42, -75.7],
-  [39.91, 116.38],
-  [9.93,-84.08]
-];
+// let mapdiv = $("#map");
+// let val1 = $(".mapbutton").first().closest('.card').find('#location').text().split(' ').join('+');
+//
+// var map;
+// var location;
+// var current;
+//
+// // jQuery ready
+// $(document).ready(function(){
+//   $(document).on('click', '.mapbutton', function() {
+//
+//     current = $(this).closest('.card').find('#location').text();
+//     console.log(current);
+//     //current = $(this).closest('.card').index();
+//     codeAddress();
+//
+//     return false;
+//   });
+//
+//   // $(document).on('click', '.mapbutton', function() {
+//   //   val1 = $(this).closest('.card').find('#location').text().split(' ').join('+');
+//   //   codeAddress();
+//   //
+//   //   return false;
+//   // });
+//
+// });
+//
+// function codeAddress() {
+//    var geocoder = new google.maps.Geocoder();
+//
+//    var address = current;
+//    console.log(current);
+//    geocoder.geocode( { 'address': address}, function(results, status) {
+//      if (status == google.maps.GeocoderStatus.OK) {
+//
+//        initMap(results[0].geometry.location.lat(), results[0].geometry.location.lng())
+//      // alert("Latitude: "+results[0].geometry.location.lat());
+//      // alert("Longitude: "+results[0].geometry.location.lng());
+//      } else {
+//        alert("Geocode was not successful for the following reason: " + status);
+//      }
+//    });
+//  }
+//
+// function initMap(lat, lng) {
+//   //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDRAAmE5TswTx5EnJdwcuUGUv_kkRqtyUM
+//
+//   location = {"lat": lat, "lng": lng};
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 7,
+//     center: location,
+//     fullscreenControl: false,
+//     minZoom: 2,
+//     maxZoom: 17,
+//   });
+//   map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+//
+//   var marker = new google.maps.Marker({
+//     position: location,
+//     map: map
+//   });
+// }
 
-// arrays
+let mapdiv = $("#map");
+let currentLoc = $(".mapbutton").first().closest('.card').find('#location').text();
+let nextLoc;
 let strArr = [];
 
-// jQuery ready
+var map;
+
+// JQuery
 $(document).ready(function(){
   $('.location').each (function () {
     strArr.push($(this).text());
   });
 
-  // When mapbutton pressed, determine index
   $(document).on('click', '.mapbutton', function() {
+      nextLoc = $(this).closest('.card').find('#location').text();
+      panMap();
 
-    current = $(this).closest('.card').index();
-    initMap();
+    return false;
   });
+
 });
 
-// ------------- initialize map -------------
+// ------------- GOOGLE MAPS API -------------
+
 function initMap() {
-  console.log(strArr.length);
-  geocodeLocs();
-  console.log(locs.length);
-  console.table(locs);
-
-  if ( locs[current] ) {
-    location = locs[current];
-  } else {
-    index = Math.floor((Math.random() * coordinates.length));
-    location = { lat : coordinates[index][0], lng : coordinates[index][1] };
-  }
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
-    center: location,
-    fullscreenControl: false,
-    minZoom: 2,
-    maxZoom: 17,
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': currentLoc}, function(results, status) {
+    if (status == "OK") {
+      var firstLocation = {"lat": results[0].geometry.location.lat(), "lng": results[0].geometry.location.lng()};
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: firstLocation,
+        fullscreenControl: false,
+        minZoom: 2,
+        maxZoom: 17,
+      });
+      map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+      addMarkers(map);
+    }
+    else {
+      if ( status == "ZERO_RESULTS" ) {
+        alert("Geocode could not find any results");
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    }
   });
-  map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
 
-  addMarkers(map);
-
-  if ( locs.length > 0 ) {
-    map.panTo(locs[current]);
-    map.setZoom( 6 );
-  } else {
-    index = Math.floor((Math.random() * coordinates.length));
-    var location = { lat : coordinates[index][0], lng : coordinates[index][1] };
-  }
 }
 
-// convert strArr to 2D array locs [latitude, longitude], create markers
-function geocodeLocs() {
+// create markers for all memories
+function addMarkers(resultsMap) {
   for (let i = 0; i < strArr.length; i++) {
-    var geocoder = new google.maps.Geocoder();
+
+    geocoder = new google.maps.Geocoder();
     geocoder.geocode({'address': strArr[i]}, function(results, status) {
       if (status === 'OK') {
-        locs[i] = results[0].geometry.location;
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
       } else {
         //alert('Geocode was not successful for the following reason: ' + status);
       }
     });
+
   }
 }
 
-function addMarkers(resultsMap) {
-  for (let i = 0; i < locs.length; i++) {
-    var marker = new google.maps.Marker({
-      map: resultsMap,
-      position: locs[i]
-    });
-  };
-}
+// pan the map to new location
+function panMap() {
 
-function geocodeSingle(str) {
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({'address': str}, function(results, status) {
-    if (status === 'OK') {
-      return results[0].geometry.location;
-    } else {
-      //alert('Geocode was not successful for the following reason: ' + status);
+  geocoder = new google.maps.Geocoder();
+  geocoder.geocode( { 'address': nextLoc}, function(results, status) {
+    if (status == "OK") {
+      var nextLocation = {"lat": results[0].geometry.location.lat(), "lng": results[0].geometry.location.lng()};
+      map.panTo( nextLocation );
+    }
+    else {
+      if ( status == "ZERO_RESULTS" ) {
+        alert("Geocode could not find any results");
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
     }
   });
+
 }
